c9a83c36c1f0bf2b8a11ef31a4ef6a2dfec3673b
palzy <test@MacBook-Pro.local>
2023-05-17 22:56:45 +0800

    penambahan detail package

--- resources/views/frontend/detailpackage.blade.php
+++ resources/views/frontend/detailpackage.blade.php
@@ -1,13 +1,16 @@
 <x-template-layout>
     <!DOCTYPE html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="build/assets/app.css">
         <script src="build/assets/script.js"></script>
-        
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -15,46 +18,79 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
     <body>
         <!-- detail -->
-     
-        <section id="location" class="pt-20 dark:bg-dark">
-            <div class="container ">
-                
 
-                <div class=" flex flex-wrap lg:text-left text-center ">
-                    <div class=" max-w-full self-end px-20 lg:w-1/2 lg:bottom-10 ">
-                        <div class="relative rounded-3xl mt-10 my-2 lg:my-10 ">
-                            <img src="dist/assets/taman kaja.png" alt="taman" class="w-max rounded-3xl mx-auto">
+        <section id="location" class="pt-20 dark:bg-dark">
+            <div class="container mx-auto px-4 py-8">
+                <div class="flex flex-col lg:flex-row text-center lg:text-left">
+                    <div class="w-full md:w-1/2 mr-16">
+                        <div class="swiper-container z-[-100] overflow-hidden">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide relative h-50   overflow-hidden rounded-lg bg-white group-hover:opacity-75 transition duration-300 ">
+                                    <img src="dist/assets/package.jpeg" alt="Gambar 1"
+                                        class="h-full w-full object-cover object-center">
+                                </div>
+                                <div class="swiper-slide relative h-50 overflow-hidden rounded-lg bg-white group-hover:opacity-75 transition duration-300 ">
+                                    <img src="dist/assets/rahwana.jpeg" alt="Gambar 2" class="w-full h-auto hidden-slide">
+                                </div>
+                                <div class="swiper-slide relative h-50 overflow-hidden rounded-lg bg-white group-hover:opacity-75 transition duration-300  ">
+                                    <img src="dist/assets/hanoman.jpeg" alt="Gambar 3" class="w-full h-auto hidden-slide">
+                                </div>
+                            </div>
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>
                         </div>
                     </div>
-                    <div class="w-full self-center dark:text-white md:flex-1 md:mr-10 mx-20 my-10 lg:my-20 lg:w-1/2">
-                        <h1 class="font-pt-serif   text-base lg:text-xl font-bold ">
-                            Reguler Package
-
-                        </h1>
-                        <p class ="text-primary text-3xl font-bold ">
-                            Rp 100.000
-                        </p>
-                        <p class=" lg:text-lg text-base font-normal pt-8  mb-8 lg:mb-20">
-                            Desa Pakraman Taman Kaja is a small village of
-                            about 140 families and is situated just to the
-                            north of Jalan Raya Ubud along Sriwedari and Sandat Street.
-                            In this village there is a pura dalem,
-                            where in this pura dalem the kecak fire dance and trance dance are performed.<br>
-
-                        </p>
-
-                        <a href="kontak2" class="text-base  font-semibold text-white bg-black 
-                        py-3 px-8 rounded-md hover:shadow-lg hover:bg-opacity-80 transition duration-300 ease-in-out">
-                            Order Ticket</a>
+                    <div class="w-full self-center dark:text-white md:flex-1 md:mr-10 mx-20 my-20 lg:my-20 lg:w-1/2">
+                        <h1 class="text-4xl font-bold mb-4 font-pt-serif">Reguler Package</h1>
+                        <div class="">
+                            <h2 class="text-2xl font-bold text-primary mb-8">Rp 100.000</h2>
+                        </div>
+                        <p class="text-gray-700 mb-16">This package is the most frequently purchased package, because it is available both online and offline. 
+                            This package is the standard package for the Kecak fire and trance dance performance.</p>
+                        <div class="mt-auto mb-4">
+                        <a href="kontak2" class="bg-dark hover:bg-primary text-white px-4 py-2 rounded transition duration-300">Buy Now</a>
+                        </div>
                     </div>
-
+                </div>
+                <h3 class="text-2xl mt-20 font-bold mb-2">Comment</h3>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div class="bg-white shadow-md rounded p-4">
+                        <blockquote>
+                            <p class="text-gray-800">"Paket ini benar-benar luar biasa! Saya sangat puas dengan layanan
+                                yang diberikan."</p>
+                            <cite class="text-gray-600">- Nama Klien 1</cite>
+                        </blockquote>
+                    </div>
+                    <div class="bg-white shadow-md rounded p-4">
+                        <blockquote>
+                            <p class="text-gray-800">"Paket ini memberikan solusi yang lengkap untuk kebutuhan saya.
+                                Sangat direkomendasikan!"</p>
+                            <cite class="text-gray-600">- Nama Klien 2</cite>
+                        </blockquote>
+                    </div>
                 </div>
             </div>
+
+            <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+            <script>
+                var swiper = new Swiper('.swiper-container', {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                });
+
+            </script>
         </section>
-        
+
         <!-- detail ends -->
     </body>
+
+
     </html>
 </x-template-layout>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\kecakcontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('kecak',[kecakcontroller::class,'index'])->name ('kecak');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
